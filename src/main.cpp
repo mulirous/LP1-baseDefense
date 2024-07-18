3eb4058a1340d503268236325433d99d094d7f4d
murilocost <muriloalimacosta@hotmail.com>
2024-07-18 19:05:27 -0300

    Teste de movimentação de personagem ao redor da tela; Ao apertar W A S D o personagem irá se movimentar, sendo printado no console a tecla apertada ou solta

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,110 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+class Character {
+    private:
+    float w; // Largura
+    float h; // Altura
+    float spd; // Velocidade
+    int life; // Vida
+    RectangleShape shape;
+    
+    public:
+
+    float posX; // Posição do personagem
+    float posY; // Posição do personagem
+
+    Character(float width, float height, float speed, int vida, float X, float Y) // Construtor
+        : w(width),
+        h(height),
+        spd(speed),
+        life(vida),
+        shape(Vector2f(width, height)) {
+        shape.setFillColor(Color::Cyan);
+        shape.setPosition(posX, posY);
+    }
+
+    void setPosition(float x, float y) { // Atualização da posição do personagem
+        posX = x;
+        posY = y;
+        shape.setPosition(posX, posY);
+    }
+
+    void goRight(){ // Ir para direita
+        if (posX > 0) { // Comparação para não sair da tela
+            posX -= spd;
+            shape.setPosition(posX, posY);
+        }
+    }
+
+    void goLeft(float wWindow){ // Ir para esquerda
+        if (posX + w < wWindow) { // Comparação para não sair da tela 
+            posX += spd;
+            shape.setPosition(posX, posY);
+        }
+    }
+
+    void goUp(){ // Ir para cima
+        if (posY > 0) { // Comparação para não sair da tela
+            posY -= spd;
+            shape.setPosition(posX, posY);
+        }
+    }
+
+    void goDown(float hWindow){ // Ir para baixo
+        if (posY + h< hWindow) { // Comparação para não sair da tela
+            posY += spd;
+            shape.setPosition(posX, posY);
+        }
+    }
+
+    void draw(RenderWindow& window) { // Desenhar o corpo do personagem na janela
+        window.draw(shape);
+    }
+};
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(VideoMode(800, 600), "Game Window");
+    Character personagem(50.f, 50.f, 8, 100, 100.f, 100.f);
 
     while (window.isOpen()) {
-        sf::Event event;
+        Event event; // Inicialização da variável que captura eventos
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
+
+            switch (event.type) {
+                case Event::Closed:
+                    window.close();
+                    break;             
+                case Event::KeyPressed: // Case para a captura de evento de teclas pressionadas
+                    if (event.key.code == Keyboard::Escape) {  // Fechar a aplicação em caso de apertar 'Esc'
+                        window.close();
+                    }
+                    cout << "Key pressed: " << event.key.code << endl;
+                    if (event.key.code == 0) { // Apertar 'A'
+                        personagem.goRight();
+                    }
+                    if (event.key.code == 3) { // Apertar 'D'
+                        personagem.goLeft(800.f);
+                    }
+                    if (event.key.code == 18) { // Apertar 'S'
+                        personagem.goDown(600.f);
+                    }
+                    if (event.key.code == 22) { // Apertar 'W'
+                        personagem.goUp();
+                    }
+                    break;
+                case Event::KeyReleased: // Case para a captura de evento de teclas soltas
+                    cout << "Key released: " << event.key.code << endl;
+                    break;
+            }
         }
 
         window.clear();
-        window.draw(shape);
+        personagem.draw(window);
         window.display();
     }
-
     return 0;
 }
