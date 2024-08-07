88fb51f92b6b9fb2bb50755cc519e04e4c3ab0c0
leirdan <uouleirdan@gmail.com>
2024-08-07 10:23:09 -0300

    feat: new constants and resources

--- src/common.h
+++ src/common.h
@@ -1,8 +1,11 @@
 #ifndef COMMON_H
 #define COMMON_H
+#include <string>
 
 const int GAMEWINDOWHEIGHT = 800;
 const int GAMEWINDOWWIDTH = 1200;
 const int PROJECTILEVELOCITY = 100;
+const std::string GAMEFONT = "resources/fonts/AGoblinAppears-o2aV.ttf";
+const std::string MENUIMAGE = "resources/assets/bg.png";
 
 #endif
